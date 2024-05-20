89d044fbe2824a74c0648299454f34e214499e3f
Lin <1030863031@qq.com>
2024-05-20 23:38:54 +0800

    完成吸顶导航交互实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,9 +2,11 @@
 import LayoutFooter from "./components/LayoutFooter.vue";
 import LayoutHeader from "./components/LayoutHeader.vue";
 import LayoutNav from "./components/LayoutNav.vue";
+import LayoutFixed from "./components/LayoutFixed.vue";
 </script>
 
 <template>
+  <LayoutFixed></LayoutFixed>
   <LayoutNav></LayoutNav>
   <LayoutHeader></LayoutHeader>
   <RouterView />
