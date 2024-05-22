c2be149115ff5bcb0f04b99b110ffbaae1e4af29
Lin <1030863031@qq.com>
2024-05-22 21:54:26 +0800

    完成新鲜好物及人气推荐动态渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -4,3 +4,13 @@ import httpInstance from '@/utils/http'
 export function getBannerAPI () {
   return httpInstance.get('/home/banner')
 }
+
+// 新鲜好物
+export function getNewAPI () {
+  return httpInstance.get('/home/new')
+}
+
+// 人气推荐
+export function getHotAPI () {
+  return httpInstance.get('/home/hot')
+}
