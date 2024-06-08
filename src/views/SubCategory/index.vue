9c5f6f72fd85749433d1a9f5ae40efd3e261587c
Lin <1030863031@qq.com>
2024-06-08 17:11:39 +0800

    二级分类-增加bug，goodList不可迭代

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -36,6 +36,18 @@ const tabChange = () => {
   reqData.value.page = 1
   getGoodList()
 }
+
+// 触底加载更多
+const disabled = ref(false)
+const load = async () => {
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  // 加载完毕，停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -49,13 +61,13 @@ const tabChange = () => {
         <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-    <div class="sub-container" @tab-change="tabChange">
-      <el-tabs v-model="reqData.sortField">
+    <div class="sub-container" >
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
         <GoodsItem v-for="good in goodList.items" :good="good" :key="good.id"></GoodsItem>
       </div>
